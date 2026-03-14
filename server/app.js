fe9948cd9ba89d76270b4c1d68549c8bdc8eb113
Raman <ramanazad881@gmail.com>
2026-03-14 03:27:28 +0300

    feat: initial socket connection between client and server

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("message", (msg) => {
+    console.log(msg.data);
+  });
+});
+
+httpServer.listen(3000);
